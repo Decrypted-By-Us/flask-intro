b35959b2674e1eb720e22000b1345d876b205f86
Carolyn Zech <carolynzech@gmail.com>
2021-03-22 20:56:35 -0400

    update

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import re
 app = Flask(__name__)
 
 
-@app.route('/palindrome/<word>')
-def is_palindrome(word):
-    if word == word[::-1]:
-        return f'{word} is a palindrome'
+@app.route('/palindrome/<text>')
+def is_palindrome(text):
+    stripped = ''.join(re.findall(r'\w+', text)).lower()
+
+    if stripped == stripped[::-1]:
+        return f'{text} is a palindrome'
     else:
-        return f'{word} is not a palindrome'
+        return f'{text} is not a palindrome'
 
 
 @app.route('/reverse/<text>')
@@ -22,26 +24,26 @@ def count_words(text):
     return str(len(re.findall(r"(?=\S*['-])([a-zA-Z'-]+)|\w+", text)))
 
 
-@app.route('/length/<word>')
-def length_string(word):
-    return str(len(word))
+@app.route('/length/<text>')
+def length_string(text):
+    return str(len(text))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == "POST":
+    if request.method == 'GET':
+        return render_template('home.html')
+    elif request.method == 'POST':
         choice = request.form.get('text-choice')
 
         if choice == 'count':
             return redirect(url_for('count_words', text=request.form.get('user-input')))
         elif choice == 'length':
-            return redirect(url_for('length_string', word=request.form.get('user-input')))
+            return redirect(url_for('length_string', text=request.form.get('user-input')))
         elif choice == 'pdrome':
-            return redirect(url_for('is_palindrome', word=request.form.get('user-input')))
+            return redirect(url_for('is_palindrome', text=request.form.get('user-input')))
         elif choice == 'rv':
             return redirect(url_for('reverse', text=request.form.get('user-input')))
-    else:
-        return render_template('home.html')
 
 
 if __name__ == '__main__':
